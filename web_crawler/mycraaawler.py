177d4a37b346b00923860ae5b238eb375933e64b
Sergio Montes <smontesleon@gmail.com>
2011-12-18 21:42:44 +0100

    NEW - task 997: Modificar el Crawler, para que reciba por parametro la url, que el usuario desee mostrar los links http://code.sidelab.es/issues/show/997 El crawler ya recibe por parametro la URL para que ser leida por el programa.

--- web_crawler/mycraaawler.py
+++ web_crawler/mycraaawler.py
@@ -3,10 +3,18 @@ import urllib2
 user_agent = " Mozilla /5.0 ( X11; U; Linux x86_64 ; en -US) AppleWebKit /534.7 (KHTML , like Gecko ) Chrome/7.0.517.41 Safari /534.7"
 _opener = urllib2 . build_opener ()
 _opener . addheaders = [( 'User - agent', user_agent )]
-url="http://www.evernote.com"
-raw_code = _opener . open ( url). read ()
+#url="http://www.evernote.com"
 
 from BeautifulSoup import BeautifulSoup as Soup
+import argparse
+
+parser = argparse . ArgumentParser ( description = "Argumentos de mi crawler" )
+parser . add_argument ( 'url' , nargs =1 ,help = 'URL de la que desea leer los links')
+argumentos = parser . parse_args()
+url_a_leer = argumentos.url.pop()
+
+raw_code = _opener . open ( url_a_leer). read ()
+
 soup_code = Soup ( raw_code )
 links = [ link ['href'] for link
  in soup_code . findAll ('a')
