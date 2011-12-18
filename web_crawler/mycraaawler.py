06327824fb4f41971455a263f5a72ad1d2d64369
Sergio Montes <smontesleon@gmail.com>
2011-12-18 21:15:16 +0100

    CLOSED - task 996: Crear la primera versión de la aplicacion http://code.sidelab.es/issues/show/996 He terminado la primera version del crawler, ya lee todos los links de la pagina www.evernote.com

--- web_crawler/mycraaawler.py
+++ web_crawler/mycraaawler.py
@@ -1,10 +1,9 @@
 
-
 import urllib2
 user_agent = " Mozilla /5.0 ( X11; U; Linux x86_64 ; en -US) AppleWebKit /534.7 (KHTML , like Gecko ) Chrome/7.0.517.41 Safari /534.7"
 _opener = urllib2 . build_opener ()
 _opener . addheaders = [( 'User - agent', user_agent )]
-url="http://www.yahoo.com"
+url="http://www.evernote.com"
 raw_code = _opener . open ( url). read ()
 
 from BeautifulSoup import BeautifulSoup as Soup
